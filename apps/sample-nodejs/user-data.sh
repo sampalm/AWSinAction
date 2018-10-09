dc6f416a967b9e232ec0f438b75cc94fa415d231
sampalm <samuelpalmeira@outlook.com>
2018-10-08 21:39:46 -0300

    removes sample-nodejs repo

--- apps/sample-nodejs/user-data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-yum -y update
-iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
-iptables -A INPUT -p tcp -m tcp --sport 80 -j ACCEPT
-iptables -A OUTPUT -p tcp -m tcp --dport 80 -j ACCEPT
