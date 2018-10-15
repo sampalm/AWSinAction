a11544a1963e851e367e47bd123b21c5374ba53f
sampalm <samuelpalmeira@outlook.com>
2018-10-15 19:47:34 -0300

    adds response struct

--- apps/sample-go-lambda/main.go
+++ apps/sample-go-lambda/main.go
@@ -7,11 +7,27 @@ import (
 )
 
 type Event struct {
-	Username string
+	Question string
 }
 
-func handler(e Event) (string, error) {
-	return fmt.Sprintf("<h1>Hello %s,</h1><h3>from the AWS Lambda GO</h3>", e.Username), nil
+type Response struct {
+	Question string
+	Answer   string
+}
+
+func handler(e Event) (Response, error) {
+	/*if len(e.Username) == 0 {
+		return "", fmt.Errorf("No Name given.")
+	}
+
+	if e.Username[0] == 'S' || e.Username[0] == 'U' {
+		return "", fmt.Errorf("We don't like %s.", e.Username)
+	}*/
+
+	return Response{
+		Question: e.Question,
+		Answer:   fmt.Sprintf("I dont know. %s", e.Question),
+	}, nil
 }
 
 func main() {
